e48b04a45abed9fcc2fe3e9ba015b78dd9dc740b
Kudinh Fc <dinhledev@gmail.com>
2022-11-25 01:21:21 -0500

    first deploy git and codepipeline :Q :q

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    user: "root", 
-    host: "localhost",
-    password: "12345",
-    database: "getlookeddb",
+    user: "admin", 
+    host: "awseb-e-kdzrcpfnxm-stack-awsebrdsdatabase-1eseaby5hc9u.cor0kbnpjcax.us-east-1.rds.amazonaws.com",
+    password: "Mapiumap1%",
+    database: "getlookedDB",
  });
 
  module.exports = db
--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cookieParser = require("cookie-parser")
 const session = require("express-session")
 const app = express();
 app.use(express.json());
-const port = 3001
+const port = process.env.PORT || 3001;
 
 //Initializing middleware
-app.use(cors({origin: ["http://localhost:3000"],methods: ["GET", "POST", "DELETE"],credentials: true}));
+app.use(cors({origin: ["http://d3gbva7za9ztwm.cloudfront.net","http://getlooked.s3-website-us-east-1.amazonaws.com"],methods: ["GET", "POST", "DELETE"],credentials: true}));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
