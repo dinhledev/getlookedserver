7914b2cab1251c46312f6317fd20b9363578a110
Kudinh Fc <dinhledev@gmail.com>
2022-12-12 23:49:00 -0500

    fix db

--- db.js
+++ db.js
@@ -1,22 +1,17 @@
 const mysql = require("mysql");
-require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     user: "admin", 
-//     host: "awseb-e-kdzrcpfnxm-stack-awsebrdsdatabase-1eseaby5hc9u.cor0kbnpjcax.us-east-1.rds.amazonaws.com",
-//     password: "Mapiumap1%",
-//     database: "getlookedDB",
-//  });
-const user =  process.env.MysqlUser
-const host =  process.env.MysqlHost
-const password =  process.env.MysqlPassword
-const database =  process.env.MysqlDatabase
-
- const db = mysql.createConnection({
-    user: user , 
-    host: host,
-    password: password,
-    database: database
+const db = mysql.createConnection({
+    user: "admin", 
+    host: "awseb-e-kdzrcpfnxm-stack-awsebrdsdatabase-1eseaby5hc9u.cor0kbnpjcax.us-east-1.rds.amazonaws.com",
+    password: "Mapiumap1%",
+    database: "getlookedDB",
  });
- console.log(db);
+
+//  const db = mysql.createConnection({
+//     user: "root", 
+//     host: "localhost",
+//     password: "12345",
+//     database: "getlookeddb",
+//  });
+
  module.exports = db
